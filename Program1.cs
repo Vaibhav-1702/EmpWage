ae032cb37781fe4adb069dc5f58db850f6308210
Vaibhav-1702 <vaibhav.koli30@gmail.com>
2024-09-25 15:26:34 +0530

    [Vaibhav] : Wages Till Condition (100 Hours or 20 Days)

--- Program1.cs
+++ Program1.cs
@@ -13,15 +13,19 @@ namespace Employee_Wage1
         public const int WAGE_PER_HOUR = 20;
         public const int FULL_DAY_HOURS = 8;
         public const int PART_TIME_HOURS = 4;
-        public const int WORKING_DAYS_PER_MONTH = 20;
+        public const int WORKING_DAYS_LIMIT = 20;
+        public const int WORKING_HOURS_LIMIT = 100;
 
         static void Main(string[] args)
         {
             int totalWage = 0;
+            int totalHoursWorked = 0;
+            int totalWorkingDays = 0;
             Random random = new Random();
 
-            for (int day = 1; day <= WORKING_DAYS_PER_MONTH; day++)
+            while (totalWorkingDays < WORKING_DAYS_LIMIT && totalHoursWorked < WORKING_HOURS_LIMIT)
             {
+                totalWorkingDays++;
                 int attendance = random.Next(0, 3);
                 int hoursWorked = 0;
 
@@ -38,11 +42,14 @@ namespace Employee_Wage1
                         break;
                 }
 
+                totalHoursWorked += hoursWorked;
                 int dailyWage = WAGE_PER_HOUR * hoursWorked;
                 totalWage += dailyWage;
             }
 
             Console.WriteLine("Total Wage for the Month: " + totalWage);
+            Console.WriteLine("Total Hours Worked: " + totalHoursWorked);
+            Console.WriteLine("Total Working Days: " + totalWorkingDays);
         }
     }
 }
