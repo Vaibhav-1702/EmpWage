50d24784995b1b04dd1b2b28e1528b6f2c96f850
Vaibhav-1702 <vaibhav.koli30@gmail.com>
2024-09-25 14:05:17 +0530

    [Vaibhav]: Present Or Absent

--- Program1.cs
+++ Program1.cs
@@ -8,9 +8,23 @@ namespace Employee_Wage1
 {
     class Program
     {
+        public const int IS_PRESENT = 1;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            Random random = new Random();
+            int attendance = random.Next(0, 2);
+
+            if (attendance == IS_PRESENT)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
+
+
