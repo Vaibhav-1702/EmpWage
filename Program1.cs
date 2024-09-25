b7e733b371a89c1c8f6b594c26165e534d95deb5
Vaibhav-1702 <vaibhav.koli30@gmail.com>
2024-09-25 15:22:47 +0530

    [Vaibhav] : Montly Wage

--- Program1.cs
+++ Program1.cs
@@ -13,30 +13,36 @@ namespace Employee_Wage1
         public const int WAGE_PER_HOUR = 20;
         public const int FULL_DAY_HOURS = 8;
         public const int PART_TIME_HOURS = 4;
+        public const int WORKING_DAYS_PER_MONTH = 20;
 
         static void Main(string[] args)
         {
+            int totalWage = 0;
             Random random = new Random();
-            int attendance = random.Next(0, 3);
 
-            int hoursWorked = 0;
-            switch (attendance)
+            for (int day = 1; day <= WORKING_DAYS_PER_MONTH; day++)
             {
-                case IS_PRESENT:
-                    hoursWorked = FULL_DAY_HOURS;
-                    Console.WriteLine("Employee is Present for Full Time");
-                    break;
-                case IS_PART_TIME:
-                    hoursWorked = PART_TIME_HOURS;
-                    Console.WriteLine("Employee is Present for Part Time");
-                    break;
-                default:
-                    Console.WriteLine("Employee is Absent");
-                    break;
+                int attendance = random.Next(0, 3);
+                int hoursWorked = 0;
+
+                switch (attendance)
+                {
+                    case IS_PRESENT:
+                        hoursWorked = FULL_DAY_HOURS;
+                        break;
+                    case IS_PART_TIME:
+                        hoursWorked = PART_TIME_HOURS;
+                        break;
+                    default:
+                        hoursWorked = 0;
+                        break;
+                }
+
+                int dailyWage = WAGE_PER_HOUR * hoursWorked;
+                totalWage += dailyWage;
             }
 
-            int dailyWage = WAGE_PER_HOUR * hoursWorked;
-            Console.WriteLine("Daily Wage is: " + dailyWage);
+            Console.WriteLine("Total Wage for the Month: " + totalWage);
         }
     }
 }
